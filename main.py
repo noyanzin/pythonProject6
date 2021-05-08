e36f0e372ca8a55f8a745e09902eb06a12aea632
Noyanzin Mark <noyanzin@inbox.ru>
2021-05-08 22:53:52 +1200

    Модифицирован вывод цвета сфетофора текстом вместо цифр

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import time
 
 
 class Color:
-    RED = 1
-    YELLOW = 2
-    GREEN = 3
+    RED = 0
+    YELLOW = 1
+    GREEN = 2
 
 
 class SingleLight:
     color: Color
     duration: int
+
+    def __str__(self):
+        c = ['RED', 'YELLOW', 'GREEN'][self.color]
+        return f"Color: {c}, duration: {self.duration}"
 
     def __init__(self, color, duration):
         self.color = color
@@ -25,20 +29,19 @@ class TrafficLight:
 
     def running(self):
         self.__timer.cancel()
-        if self.__light_number == 3:
-            self.__light_number = 1
+        if self.__light_number == 2:
+            self.__light_number = 0
         else:
             self.__light_number += 1
-        print(f'light_number = {self.__light_number}')
         light = self.__lights[self.__light_number]
-        print(f'{time.time()}: color: {light.color}, duration: {light.duration}')
+        print(f'{time.time()}:light_number = {self.__light_number} {light}')
         self.__timer = threading.Timer(light.duration, self.running)
         self.__timer.start()
 
     def __init__(self):
-        self.__lights[1] = SingleLight(Color.RED, 7)
-        self.__lights[2] = SingleLight(Color.YELLOW, 2)
-        self.__lights[3] = SingleLight(Color.GREEN, 5)
+        self.__lights[0] = SingleLight(Color.RED, 7)
+        self.__lights[1] = SingleLight(Color.YELLOW, 2)
+        self.__lights[2] = SingleLight(Color.GREEN, 5)
         self.__timer = threading.Timer(self.__lights[1].duration, self.running)
         self.__timer.start()
 
